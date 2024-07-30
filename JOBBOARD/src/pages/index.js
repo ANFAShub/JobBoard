ac0ac1a51103f23866e437950f2ec47e505156df
Anfas03 <anfasxwork@gmail.com>
2024-07-30 15:24:23 +0530

    setup

---
+++ JOBBOARD/src/pages/index.js
@@ -0,0 +1,20 @@
+import Findjobs from "./Findjobs";
+import AuthPage from "./Auth";
+import Companies from "./Companies";
+import UserProfile from "./UserProfile";
+import CompanyProfile from "./CompanyProfile";
+import Uploadjob from "./Uploadjob";
+import About from "./About";
+import JobDetail from "./JobDetail";
+
+export {
+    Findjobs,
+    AuthPage,
+    Companies,
+    UserProfile,
+    CompanyProfile,
+    Uploadjob,
+    About,
+    JobDetail,
+};
+
